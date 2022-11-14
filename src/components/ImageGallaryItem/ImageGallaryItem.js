c849071bd29bcb0542593792e2d1e518215fd71f
Aleksandr Cheverda <alexar@ua.fm>
2022-11-14 19:18:38 +0200

    upd files

--- src/components/ImageGallaryItem/ImageGallaryItem.js
+++ src/components/ImageGallaryItem/ImageGallaryItem.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-import { LineWave } from 'react-loader-spinner';
 import { Image } from './ImageGallaryItem.Styled';
 
 const ImageGallaryItem = ({ smImg, tags, onClick }) => {
