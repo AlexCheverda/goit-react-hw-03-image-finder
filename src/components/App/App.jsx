3aa2b2ade09ef106b220afeb9f6ad73c66ac83b6
Aleksandr Cheverda <alexar@ua.fm>
2022-11-14 21:03:56 +0200

    upd App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/react-toastify';
+import { Container } from './App.Styled';
+import { fetchImages } from "Api/Api";
+import { mapPictures } from "Api/mapPictures";
+import Searchbar from "components/Searchbar";
+import ImageGallary from "components/ImageGallary";
+import Button from "components/Button";
+import Modal from "components/Modal";
+import Loader from "components/Loader";
+
+class App extends Component {
+  state = {
+    inputValue: '',
+    images: [],
+    page: 1,
+    isLoading: false,
+    showModal: null,
+    error: null,
+  };
+  componentDidUpdate(_, prevState) {
+    const prevSearch = prevState.inputValue;
+    const currentSearch = this.state.inputValue;
+    const prevPage = prevState.page;
+    const currentPage = this.state.page;
+
+    if (prevSearch !== currentSearch || prevPage !== currentPage) {
+      this.updateImages();
+    }
+  }
+  
+}
